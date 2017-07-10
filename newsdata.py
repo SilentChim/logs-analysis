2efb568f8f11287896783934db0c1f90a36069e4
Peyton Lucas <plucas@Nomad.local>
2017-07-10 08:22:13 -0500

    Create for loops to print query results

--- newsdata.py
+++ newsdata.py
@@ -5,29 +5,48 @@ def connect():
   return psycopg2.connect("dbname=news")
 
 def topArticles():
-  """Answers which are the top three articles of all time.
-  Use COUNT as SUM(*), LIMIT, ORDER BY, GROUP BY, WHERE in SELECT statements"""
-  DB = connect()
-  c = DB.cursor()
-  ariticles = """SELECT * FROM top_articles;"""
-  c.execute(articles,)
-  DB.commit()
-  DB.close()
+    """ Answers which are the top three articles of all time."""
+    DB = connect()
+    c = DB.cursor()
+    c.execute("""SELECT * FROM top_articles;""")
+    articles = c.fetchall()
+    DB.commit()
+    DB.close()
+    return articles
+
+articles = topArticles()
+print ("The three most popular articles are:\n")
+for article in articles:
+    print (article[0] + " - " + str(article[1])) + " views"
+print ("\n")
 
 def topAuthors():
-  """Answers who are the top four authors of all time.
-  Use COUNT as SUM(*), LIMIT, ORDER BY, GROUP BY, WHERE in SELECT statements"""
-  DB = connect()
-  c = DB.cursor()
-  author = """SELECT * FROM top_authors;"""
-  c.execute(authors,)
-  DB.commit()
-  DB.close()
+    """ Answers who are the top four authors of all time."""
+    DB = connect()
+    c = DB.cursor()
+    c.execute("""SELECT * FROM top_authors;""")
+    authors = c.fetchall()
+    DB.commit()
+    DB.close()
+    return authors
+
+authors = topAuthors()
+print ("The three most popular authors are:\n")
+for author in authors:
+    print (str(author[0]) + " - " + str(author[1])) + " views"
+print ("\n")
 
 def errorRequests():
-  """Answers which days led to more than 1 percent of request errors."""
-  DB = connect()
-  c = DB.cursor()
-  c.execute("""  """)
-  DB.commit()
-  DB.close()
+    """ Answers which days led to more than 1 percent of request errors."""
+    DB = connect()
+    c = DB.cursor()
+    c.execute("""SELECT * FROM request_errors;""")
+    errors = c.fetchall()
+    DB.commit()
+    DB.close()
+    return errors
+
+errors = errorRequests()
+print ("The days with more than 1 percent of errors are:\n")
+for error in errors:
+    print (str(error[0]) + " - " + str(error[1])) + "%"
